d82e01ca8a4228f12b5f86da9b29cb10eda9bc29
Kellbis <holasoykellbis@gmail.com>
2024-11-06 15:27:14 -0400

    useNavigate

--- src/main.jsx
+++ src/main.jsx
@@ -63,11 +63,3 @@ createRoot(document.getElementById('root')).render(
 		<RouterProvider router={router} />
 	</StrictMode>
 );
-
-// In short, what did I do? Because I forget start git and commits changes and what I learned.
-// 1. I created a new project with vite and react using `npm create vite@latest react-router --template react`.
-// 2. I installed react-router-dom with `npm install --save react-router-dom`.
-// 3. I learned about routers types, Browser Router, Hash Router and Memory Router.
-// 4. Using react router documentation I created a new router with createHashRouter in this file and I established some routes.
-// 5. How to use Link and NavLink components to navigate between pages.
-// 6. How to use useParams hook to get the url parameter.
--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import { blogData } from '../data/blogData';
 
 function BlogPost() {
+	const navigate = useNavigate();
 	const { url } = useParams();
 
 	const post = blogData.find((post) => post.url === url);
 
-	console.log(post);
+	// console.log(post);
 
 	if (!post) {
 		console.log(post);
 		return <div>Blog post not found</div>;
 	}
 
+	const returnToBlog = () => {
+		navigate('/blog');
+	};
+
 	return (
 		<>
 			<h2>{post.title}</h2>
+			<button onClick={returnToBlog}>Return To Blog</button>
 			<p>{post.content}</p>
 			<p>{post.author}</p>
 		</>
