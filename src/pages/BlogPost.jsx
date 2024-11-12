d56ccefa6e0922309dd58804373170375e5eeeec
Kellbis <holasoykellbis@gmail.com>
2024-11-11 20:11:51 -0400

    component composition without rcontext

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { blogData } from '../data/blogData';
 import { useAuth } from '../auth/auth';
@@ -16,32 +17,67 @@ function BlogPost() {
 		return <div>Blog post not found</div>;
 	}
 
-	const adminPowers = post.author && (auth.user?.isAdmin || post.author === auth.user?.username);
+	const adminUser = post.author && (auth.user?.isAdmin || post.author === auth.user?.username);
 	const editorUser = post.author && auth.user?.isEditor;
 
 	const returnToBlog = () => {
 		navigate('/blog');
 	};
 
-	return (
-		<>
-			<h2>{post.title}</h2>
-			<button onClick={returnToBlog}>Return To Blog</button>
-			<p>{post.content}</p>
-			<p>{post.author}</p>
+	const [editing, setEditing] = useState(false);
+	const [clear, setClear] = useState(false);
+	const [title, setTitle] = useState(post.title);
+	const [content, setContent] = useState(post.content);
 
-			{adminPowers && (
+	const [author, setAuthor] = useState(post.author);
+
+	const saveChanges = () => {
+		// Logic to save changes to the post
+		if (title === '' || content === '' || author === '') {
+			window.alert('Fields cannot be empty.');
+		} else {
+			post.title = title;
+			post.content = content;
+			post.author = author;
+			setEditing(false);
+		}
+	};
+	const deletePost = () => {
+		const index = blogData.findIndex((post) => post.slug === slug);
+		if (index !== -1) {
+			blogData.splice(index, 1);
+			setClear(true);
+			navigate('/blog');
+		}
+	};
+
+	return (
+		<div>
+			{editing ? (
 				<>
-					<button>Delete</button>
-					<button>Edit</button>
+					<input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+					<textarea value={content} onChange={(e) => setContent(e.target.value)} />
+					<input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} />
+					<button onClick={saveChanges}>Save</button>
+				</>
+			) : (
+				<>
+					<h2>{title}</h2>
+					<p>{content}</p>
+					<p>Author: {author}</p>
 				</>
 			)}
-			{editorUser && (
+
+			<button onClick={returnToBlog}>Return To Blog</button>
+
+			{adminUser && (
 				<>
-					<button>Edit</button>
+					<button onClick={deletePost}>Delete</button>
+					<button onClick={() => setEditing(true)}>Edit</button>
 				</>
 			)}
-		</>
+			{editorUser && !adminUser && <button onClick={() => setEditing(true)}>Edit</button>}
+		</div>
 	);
 }
 
